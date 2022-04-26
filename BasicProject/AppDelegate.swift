2ae359e0048b428563b88b73f50e3ed60b41aaf6
Kirill Ki <kirillkalapov@gmail.com>
2022-04-26 19:09:17 +0200

    Add onboarding

--- BasicProject/AppDelegate.swift
+++ BasicProject/AppDelegate.swift
@@ -12,12 +12,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     var window: UIWindow?
+    let onboardingContainerViewController = OnboardingContainerViewController()
+    let tabBarController = UITabBarController()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.backgroundColor = .systemBackground
+        onboardingContainerViewController.delegate = self
+        
+        
         
         let vc1 = SearchViewController()
         let vc2 = ContactsViewController()
@@ -31,10 +36,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let nc2 = UINavigationController(rootViewController: vc2)
         let nc3 = UINavigationController(rootViewController: vc3)
         
-        let tabBarController = UITabBarController()
+        
         tabBarController.viewControllers = [nc1, nc2, nc3]
         
-        window?.rootViewController = tabBarController
+        checkFirstLaunch()
+        
+        window?.rootViewController = onboardingContainerViewController
         
         return true
     }
@@ -62,9 +69,42 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
     }
 
-
-
-
-
 }
 
+extension AppDelegate {
+    func checkFirstLaunch() {
+        if LocalState.hasOnboarded {
+            setRootViewController(tabBarController)
+        } else {
+            setRootViewController(onboardingContainerViewController)
+            
+        }
+    }
+}
+
+extension AppDelegate: OnboardingContainerViewControllerDelegate {
+    func didFinishOnboarding() {
+        LocalState.hasOnboarded = true
+        setRootViewController(tabBarController)
+        
+    }
+}
+
+//MARK: - Slow transition
+extension AppDelegate {
+    func setRootViewController(_ vc: UIViewController, animated: Bool = true) {
+        guard animated, let window = self.window else {
+            self.window?.rootViewController = vc
+            self.window?.makeKeyAndVisible()
+            return
+        }
+        
+        window.rootViewController = vc
+        window.makeKeyAndVisible()
+        UIView.transition(with: window,
+                          duration: 0.8,
+                          options: .transitionCrossDissolve,
+                          animations: nil,
+                          completion: nil)
+    }
+}
